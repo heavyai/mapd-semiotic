b896246575ce6c52a8c2ede88b2d8b8fc7673a98
Chris Henrick <chrishenrick@gmail.com>
2018-06-15 15:58:57 -0700

    fixed fetchData action

--- src/actions/requestData.js
+++ src/actions/requestData.js
@@ -1,9 +1,9 @@
 import { REQUEST_DATA } from "../common/actionTypes"
 import { queryMapd } from "../services/connector"
 
-export const fetchData = query => dispatch => ({
+export const fetchData = (connection, query) => dispatch => dispatch({
   type: REQUEST_DATA,
-  payload: queryMapd(query)
+  payload: queryMapd(connection, query)
 })
 .catch(error => {
   console.error(error)
