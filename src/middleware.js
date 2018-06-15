1440abae2575e5ddf2716dfcb744201526a93a1a
Chris Henrick <chrishenrick@gmail.com>
2018-06-15 14:31:32 -0700

    use redux-thunk middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,8 @@
 import promiseMiddleware from 'redux-promise-middleware'
-import { createLogger } from 'redux-logger';
+import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk'
 
-const middleware = [ promiseMiddleware() ]
+const middleware = [ thunk, promiseMiddleware() ]
 
 if (process.env.NODE_ENV === "development") {
   middleware.push(createLogger())
