b3d2e000fc619d1c8fc811f13d4ac032ea1023f7
Chris Henrick <chrishenrick@gmail.com>
2018-06-29 15:55:21 -0700

    added "piece hover annotation" to stacked bar

--- src/components/StackedBar.js
+++ src/components/StackedBar.js
@@ -2,8 +2,12 @@ import React from "react"
 import PropTypes from "prop-types"
 import { OrdinalFrame } from "semiotic"
 import { schemeSet1 } from 'd3-scale-chromatic'
+import { format } from 'd3-format'
 
 import '../styles/chart-default-styles.css'
+import '../styles/StackedBar.css'
+
+const formatter = format(".4s")
 
 const airlines = [
   'Mesa Airlines',
@@ -18,7 +22,7 @@ const kvArray = airlines.reduce((acc, cur, i) => {
   return acc
 }, [])
 
-const colorHash = new Map(kvArray)
+const colorMap = new Map(kvArray)
 
 const StackedBar = ({ data }) => {
   data = data.filter(d => d.key1 !== "undefined")
@@ -27,15 +31,26 @@ const StackedBar = ({ data }) => {
     <div className="chart stacked-bar">
       <OrdinalFrame
         title={'Avg Arrdelay By Dest_city, Carrier_name'}
-        size={[700, 400]}
+        size={[1400, 400]}
         data={data}
-        style={d => ({ fill: colorHash.get(d.key1), stroke: '#fff' })}
+        style={d => ({ fill: colorMap.get(d.key1), stroke: '#fff' })}
         oAccessor={"key0"}
         rAccessor={"val"}
         type="bar"
         axis={{ orient: "left", label: "avg arrdelay"}}
         margin={{ left: 80, bottom: 100, right: 10, top: 40 }}
         oPadding={5}
+        oLabel={d => (
+          <text transform="translate(-15,0) rotate(45)">{d}</text>
+        )}
+        pieceHoverAnnotation={true}
+        tooltipContent={d => (
+          <div className="tooltip-content">
+            <p>{d.key0}</p>
+            <p>{d.key1}</p>
+            <p>{formatter(d.val)}</p>
+          </div>
+        )}
       />
     </div>
   )
