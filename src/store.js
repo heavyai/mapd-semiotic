28bf09d1f74ab0008ff33f7871fe0588425c2b00
Chris Henrick <chrishenrick@gmail.com>
2018-07-27 11:43:23 -0700

    added redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
-import { createStore, applyMiddleware } from "redux"
+import { createStore, applyMiddleware, compose } from "redux"
 import reducer from "./reducers"
 import middleware from "./middleware"
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   reducer,
-  applyMiddleware(...middleware)
+  composeEnhancers(applyMiddleware(...middleware))
 )
 
 export default store
